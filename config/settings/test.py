fb6bae35d6f4cfe09c26d023449ba51e9c150f83
choiseyeon <choi20014830@gmail.com>
2024-01-23 21:14:14 +0900

    fix: 테스트

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,12 +1,12 @@
-from .base_settings import *
+from config.settings import *
 
-DATABASES = {
-    "default": {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('TEST_DATABASE_NAME'),
-        'USER': env('TEST_DATABASE_USER'),
-        'PASSWORD': env('TEST_DATABASE_PASSWORD'),
-        'HOST': env('TEST_DATABASE_HOST'),
-        'PORT': env('TEST_DATABASE_PORT'),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': env('TEST_DATABASE_NAME'),
+#         'USER': env('TEST_DATABASE_USER'),
+#         'PASSWORD': env('TEST_DATABASE_PASSWORD'),
+#         'HOST': env('TEST_DATABASE_HOST'),
+#         'PORT': env('TEST_DATABASE_PORT'),
+#     }
+# }
