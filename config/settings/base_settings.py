5030e0a1a260e576c6401f34ca37d255bf2b22b2
choiseyeon <choi20014830@gmail.com>
2024-01-23 17:33:53 +0900

    fix: 세팅값 수정

--- config/settings/base_settings.py
+++ config/settings/base_settings.py
@@ -14,7 +14,7 @@ import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 env = environ.Env(DEBUG=(bool, True))
 
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
